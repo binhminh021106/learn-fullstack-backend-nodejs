65d3283c74bfd1eea8f2a3966dde4c802b4f30cd
BinhMinh <minhdzwama211@gmail.com>
2026-02-28 22:15:21 +0700

    nodejs express + env

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOST_NAME || 'localhost';
+
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/shop", (req, res) => {
+  // res.send("Hi con cặc!");
+  res.render("sample.ejs");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`);
+});
