dfaf611ebb2230a79efc12891b35a50b16e9c8eb
BinhMinh <minhdzwama211@gmail.com>
2026-03-09 21:17:43 +0700

    update connection Sql part 2

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+// Connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3307,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -15,17 +15,8 @@ configViewEngine(app);
 app.use("/", webRoutes);
 
 // test connection
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
 connection.query("SELECT * FROM Users", function (err, results, fields) {
   console.log(results);
-  console.log("fields: ", fields);
 });
 
 app.listen(port, hostname, () => {
